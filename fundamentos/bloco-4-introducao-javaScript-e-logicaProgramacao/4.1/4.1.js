91a73fec019b618b17f96fa79ea2cf6f4f06e83b
Flávia Labanca <flavialabanca@gmail.com>
2021-07-30 18:48:24 -0300

    exercicio 2

--- fundamentos/bloco-4-introducao-javaScript-e-logicaProgramacao/4.1/4.1.js
+++ fundamentos/bloco-4-introducao-javaScript-e-logicaProgramacao/4.1/4.1.js
@@ -1,10 +1,32 @@
+// exercício 1
+
 const myName = "Flávia";
 const birthCity = "Rio de Janeiro";
 var birthYear = 1983;
 
 birthYear = 2030;
-birthCity = "manaus";
 
+console.log ('exercício um:');
 console.log (myName);
 console.log (birthCity);
 console.log (birthYear);
+console.log ('-----------');
+
+// exercício 2
+
+const base = 5;
+let height = 8;
+const area = base * height;
+const perimeter = (base * 2) + (height * 2)
+
+console.log ('exercício dois:');
+console.log (base);
+console.log (height);
+console.log (area);
+console.log (perimeter);
+console.log ('--------------');
+
+
+
+
+// Crie uma constante chamada perimeter e atribua a ela a soma de todos os lados do retângulo.
