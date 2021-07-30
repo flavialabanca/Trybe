93e717b820357397ccdd03074ef982e6215a3ee4
Flávia Labanca <flavialabanca@gmail.com>
2021-07-30 18:36:45 -0300

    começa exercicios bloco 4

---
+++ fundamentos/bloco-4-introducao-javaScript-e-logicaProgramacao/4.1/4.1.js
@@ -0,0 +1,10 @@
+const myName = "Flávia";
+const birthCity = "Rio de Janeiro";
+var birthYear = 1983;
+
+birthYear = 2030;
+birthCity = "manaus";
+
+console.log (myName);
+console.log (birthCity);
+console.log (birthYear);
