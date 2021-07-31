df965a1b7eb1802e70295f8cb0a5d9d1d54ddc79
Flávia Labanca <flavialabanca@gmail.com>
2021-07-31 00:29:21 -0300

    exercicio 5

--- fundamentos/bloco-4-introducao-javaScript-e-logicaProgramacao/4.1/4.1.js
+++ fundamentos/bloco-4-introducao-javaScript-e-logicaProgramacao/4.1/4.1.js
@@ -28,19 +28,82 @@ console.log ('--------------');
 
 // exercício 3
 
-const nota = 75;
+const nota = 69;
 let mensagem;
+let estado;
 
 if (nota >= 80){
     mensagem = 'Parabéns, você foi aprovado'
+    estado = 'aprovado'
 }
 else if (nota < 80 && nota >= 60) {   
     mensagem = 'Você está na nossa lista de espera'
+    estado = 'lista'
 }
 else if (nota < 60){
     mensagem = 'Você foi reprovado'
+    estado = 'reprovado'
 }   
 
 console.log ('exercício três:');
 console.log (mensagem);
+console.log ('---------------');
+
+// exercício 4
+
+const currentHour = 22;
+let message = "";
+
+if (currentHour >= 22){
+    message = 'não deveríamos comer nada, é hora de dormir';
+}
+else if (currentHour >= 18 && current < 22){
+    message = 'rango da noite, vamos jantar :D';
+}
+else if (currentHour >=14 && currentHour < 18){
+    message = 'vamos fazer um bolo pro café da tarde?';
+}
+else if (currentHour > 11 && currentHour < 14){
+    message = 'hora do almoço';
+}
+else if (currentHour > 4 && currentHour < 11){
+    message = 'hummm, cheiro de café recém passado';
+}
+
+// segunda parte
+
+let weekDay = 'sabado';
+
+if (weekDay === 'segunda feira' || weekDay === 'terça feira' || weekDay === 'quarta feira' || weekDay === 'quinta feira' || weekDay === 'sexta feira'){
+    console.log ('Oba, mais um dia de aprendizado na Trybe! :D');
+}
+else {
+    console.log ('FINALMENTE, descanso merecido UwU');
+}
+
+console.log ('exercício quatro:');
+console.log (message);
+console.log ('---------------');
+
+// exercício 5
+
+//let estado;
+
+console.log ('exercício cinco:');
+console.log (nota);
+
+switch (estado){
+    case ('aprovado'):
+        console.log('aprovada');
+        break;
+    case ('lista'):
+        console.log('lista');
+        break;
+    case ('reprovado'):
+        console.log('reprovada')
+        break;
+    default:
+        console.log('não se aplica')
+}
+
 console.log ('---------------');
