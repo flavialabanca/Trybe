fd7cd6381bbe08d9efc70affc1a1f0a5c65ef901
Flávia Labanca <flavialabanca@gmail.com>
2021-08-01 20:30:29 -0300

    exercicios ate o 6

--- fundamentos/bloco-4-introducao-javaScript-e-logicaProgramacao/4.2/4.2.js
+++ fundamentos/bloco-4-introducao-javaScript-e-logicaProgramacao/4.2/4.2.js
@@ -69,3 +69,56 @@
 //}
 
 // Exercícios do Course: Vamos Praticar! 
+
+//1
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+//console.log('--- ex um');
+for (let index = 0; index < numbers.length; index +=1){
+//        console.log(numbers[index]);
+}
+
+//console.log('--- ex dois');
+let total = 0;
+for (let index = 0; index < numbers.length; index +=1){
+    total = total + numbers[index];
+}
+//console.log(total);
+
+//console.log('--- ex tres');
+let media = 0;
+media = total/numbers.length;
+//console.log(media);
+
+//console.log('--- ex quatro');
+if (media > 20){
+//    console.log('valor maior que 20!');
+} else {
+//    console.log('valor menor ou igual a 20');
+}
+
+//console.log('--- ex cinco');
+let maiorNumero = 0;
+for (let index = 0; index < numbers.length; index +=1){
+    if (numbers[index] > maiorNumero){
+        maiorNumero = numbers[index];
+    }
+}
+//console.log('maior numero: ' + maiorNumero);
+
+console.log('--- ex seis');
+let numerosImpares = 0;
+for (let index = 0; index <numbers.length; index +=1){
+    if (numbers[index] % 2 !== 0){
+        numerosImpares +=1;
+    } 
+}
+if (numerosImpares === 0){
+    console.log('não tem nenhum impar');
+} else {
+    console.log(numerosImpares + ' numeros impares.')
+}
+
+console.log(' ');
+console.log('--- ex sete');
