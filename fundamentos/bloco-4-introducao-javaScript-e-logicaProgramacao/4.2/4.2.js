4c5e094d547b194598f57ba0bf29efb0a3f3ddc9
Flávia Labanca <flavialabanca@gmail.com>
2021-08-01 18:23:44 -0300

    exercicios ARRAY

---
+++ fundamentos/bloco-4-introducao-javaScript-e-logicaProgramacao/4.2/4.2.js
@@ -0,0 +1,39 @@
+// aula do gus:
+//let pessoas = ['flavia', 'fernanda', 'julio', 'vitor'];
+//console.log(pessoas);
+//console.log(pessoas.sort());
+//pessoas.push ('vô raul');
+//console.log(pessoas);
+//console.log('tamanho do array: ' + pessoas.length)
+
+//for (let index = 0; index < pessoas.length; index++){
+//    console.log(pessoas[index]);
+//}
+
+//pessoas.pop();
+//console.log('--- depois do pop')
+//for (let index = 0; index < pessoas.length; index++){
+//    console.log(pessoas[index]);
+//}
+
+//let indexPessoas = pessoas.indexOf('vitor');
+//console.log(indexPessoas);
+
+// exercícios - ARRAY:
+// 1
+
+//let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+//indexMenu = menu.indexOf('Serviços');
+//console.log(indexMenu);
+
+// 2
+
+//indexMenu = menu.indexOf('Portfólio');
+//console.log(indexMenu);
+
+// 3 
+
+//console.log(menu);
+//menu.push('Contato');
+//console.log(menu.length);
+//console.log(menu);
