18df2c8a38a04807dfebc8444f480e7bbf03ea8a
Flávia Labanca <flavialabanca@gmail.com>
2021-08-01 23:17:14 -0300

    starta exercicios 4.3

---
+++ fundamentos/bloco-4-introducao-javaScript-e-logicaProgramacao/4.3/4.3.js
@@ -0,0 +1,11 @@
+// dado N, escrever um quadrado de * com N linhas e N colunas
+
+let n = 5;
+let linhaNova = '';
+
+for (let index = 0; index < n; index += 1){
+//    console.log('imprimiu uma linha');
+    for (let indexDois = 0; indexDois < n; indexDois += 1){
+        linhaNova = linhaNova + '*'
+    }
+}
